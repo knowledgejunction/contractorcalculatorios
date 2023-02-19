01f95a4b4f0bb93458e7de2230d358ef70eb3649
ssriva11 <ssriva11@ford.com>
2023-02-19 13:20:50 -0500

    intial commit

--- contractorcalculator/ViewController.swift
+++ contractorcalculator/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculateBtn: UIButton!
+    @IBOutlet weak var materialTxt: UITextField!
+    @IBOutlet weak var laborTxt: UITextField!
+    
+    @IBOutlet weak var totalLbl: UILabel!
+    @IBOutlet weak var taxLbl: UILabel!
+    @IBOutlet weak var subTotalLbl: UILabel!
+    
+    @IBAction func calculate(_ sender: Any) {
+        let laborValue = Double(laborTxt.text!) ?? 0.0
+        let materialValue = Double(materialTxt.text!) ?? 0.0
+        
+        let subTotal = laborValue + materialValue
+        let tax = subTotal * 0.05
+        let total = subTotal + tax
+        
+        subTotalLbl.text = "\(subTotal.description)"
+        taxLbl.text = "\(tax.description)"
+        totalLbl.text = "\(total.description)"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
