0b27b070f89b7bfe85c8c0c6aade90f99d275649
ssriva11 <ssriva11@ford.com>
2023-02-19 04:17:54 -0500

    Initial Commit

---
+++ contractorcalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contractorcalculator
+//
+//  Created by Srivastav, Suraj (S.K.) on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
